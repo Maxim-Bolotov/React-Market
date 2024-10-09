7e610a1d9157287ed2c8c52bcfecd2366af2fd4c
Bolotov Maxim <maximbolotov@eventagrate.com>
2024-10-10 01:28:50 +0300

    add stripe

--- src/index.js
+++ src/index.js
@@ -1,25 +1,29 @@
 import React from "react";
-import { render } from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
+import { Elements } from "@stripe/react-stripe-js";
+import { createRoot } from "react-dom/client";
 
 import App from "./App";
 import { store, persistor } from "./store/store";
+import { stripePromise } from "./utils/stripe/stripe.utils";
 
 import "./index.scss";
 
 const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
 
-render(
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <BrowserRouter>
-          <App />
+          <Elements stripe={stripePromise}>
+            <App />
+          </Elements>
         </BrowserRouter>
       </PersistGate>
     </Provider>
-  </React.StrictMode>,
-  rootElement
+  </React.StrictMode>
 );
